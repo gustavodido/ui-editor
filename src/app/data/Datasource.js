8aa16ed3fb10cf4dbdec885541d8d994b60781ab
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-02 15:31:52 -0800

    Add basic columns.

--- src/app/data/Datasource.js
+++ src/app/data/Datasource.js
@@ -2,26 +2,104 @@ class Datasource {
     getColumns() {
         return [
             {
-                key: 'id',
-                name: 'ID'
+                key: 'isReleased',
+                name: 'RELEASED'
             },
             {
-                key: 'title',
-                name: 'Title'
+                key: 'brand',
+                name: 'BRAND'
             },
             {
-                key: 'count',
-                name: 'Count'
+                key: 'market',
+                name: 'MARKET'
+            },
+            {
+                key: 'merchHier',
+                name: 'MERCH HIER'
+            },
+            {
+                key: 'ccNumber',
+                name: 'CC NUMBER'
+            },
+            {
+                key: 'isd',
+                name: 'IN STORE DATE'
+            },
+            {
+                key: 'mkd',
+                name: 'MARKDOWN DATE'
+            },
+            {
+                key: 'distros',
+                name: 'DISTROS'
+            },
+            {
+                key: 'aps',
+                name: 'APS'
+            },
+            {
+                key: 'paps',
+                name: 'PROMOTED APS'
+            },
+            {
+                key: 'cp',
+                name: 'CYCLE PERIOD'
+            },
+            {
+                key: 'sp',
+                name: 'SAFETY PERIOD'
+            },
+            {
+                key: 'iaf',
+                name: 'IAF'
+            },
+            {
+                key: 'iafExpiration',
+                name: 'IAF EXPIRATION'
+            },
+            {
+                key: 'visMin',
+                name: 'VIS MIN'
+            },
+            {
+                key: 'visMinPeriod',
+                name: 'VIS MIN PERIOD'
+            },
+            {
+                key: 'ti',
+                name: 'TI'
+            },
+            {
+                key: 'delta',
+                name: 'Δ'
             }
         ];
     }
 
     getData() {
         let rows = [];
-        for (let i = 1; i < 1000; i++) {
+        for (let i = 1; i < 100; i++) {
             rows.push({
                 id: i,
-                title: 'Title ' + i,
+                brand: 'GAP',
+                market: 'US',
+                merchHier: 'HOT CHESTNUT',
+                isReleased: false,
+                ccNumber: '000000000' + i,
+                isd: '11/11/2018',
+                mkd: '18/11/2018',
+                distros: 'TIER 2 (251)',
+                dcs: '0020 TDC (151)',
+                aps: 0,
+                paps: 0,
+                cp: 7,
+                sp: 40,
+                iaf: 0,
+                iafExpiration: '',
+                visMin: 1,
+                visMinPeriod: '',
+                ti: 100,
+                delta: 0,
                 count: i * 1000
             });
         }
