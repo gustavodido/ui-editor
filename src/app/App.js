9c80942c870ba8bf5d4c414c1849db3e6c30415e
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-12-02 00:24:15 -0800

    Add body table.

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Table} from 'reactstrap';
 import './App.css';
 import NavigationBar from "./containers/NavigationBar";
 
@@ -9,9 +9,38 @@ class App extends Component {
             <div>
                 <NavigationBar/>
                 <Container fluid={true}>
-                    <Row>
+                    <Row noGutters={true}>
                         <Col>
-
+                            <Table bordered striped>
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Username</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                </tbody>
+                            </Table>
                         </Col>
                     </Row>
                 </Container>
